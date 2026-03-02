0aa951737d1bcacea65b83f8d18c1e8ec14ad233
Alexander Saadia <asaadiajinich@lcs.on.ca>
2026-03-02 14:03:24 -0500

    Initial Commit

---
+++ MyBigApp/MyBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigAppApp.swift
+//  MyBigApp
+//
+//  Created by Alexander Saadia on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MyBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
